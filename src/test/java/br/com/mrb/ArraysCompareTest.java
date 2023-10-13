28584fa5424ff93de80d7c44587ab42841e713d8
mauro <mrb0305@gmail.com>
2023-10-12 21:06:08 -0300

    Comparando Arrays

---
+++ src/test/java/br/com/mrb/ArraysCompareTest.java
@@ -0,0 +1,19 @@
+package br.com.mrb;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class ArraysCompareTest {
+
+    @Test
+    void test(){
+        int[] numbers = {25,8,21,32,3};
+        int[] expectedArray = {3,8,21,25,32};
+
+        Arrays.sort(numbers);
+
+        assertArrayEquals(numbers, expectedArray);
+    }
+}
