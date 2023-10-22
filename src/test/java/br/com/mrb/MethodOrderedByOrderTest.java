44448064cc7fc8c8d2fed00f6e7ea454f4b18ecf
mauro <mrb0305@gmail.com>
2023-10-22 20:12:51 -0300

    Defenir configuração de ordenação de execução de classes

--- src/test/java/br/com/mrb/MethodOrderedByOrderTest.java
+++ src/test/java/br/com/mrb/MethodOrderedByOrderTest.java
@@ -1,34 +1,44 @@
 package br.com.mrb;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
-@Order(1)
+//@Order(1)
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodOrderedByOrderTest {
+
+    StringBuilder builder = new StringBuilder();
+
+    @AfterEach
+    public void  afterEach(){
+        System.out.println("The actual value is: " + builder);
+    }
 
     @Test
     @Order(0)
     void testC(){
+        builder.append("1");
         System.out.println("Running Teste C");
     }
 
     @Test
     @Order(1)
     void testD(){
+        builder.append("2");
         System.out.println("Running Teste D");
     }
     @Test
     @Order(2)
     void testA(){
+        builder.append("3");
         System.out.println("Running Teste A");
     }
 
     @Test
     @Order(3)
     void testB(){
+        builder.append("4");
         System.out.println("Running Teste B");
     }
 
