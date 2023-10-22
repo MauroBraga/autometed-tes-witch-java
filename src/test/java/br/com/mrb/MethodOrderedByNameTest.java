9a3abaa04494065c4abc3ae60d40a26ed04df4ea
mauro <mrb0305@gmail.com>
2023-10-22 16:41:27 -0300

    Ordenação por nome

---
+++ src/test/java/br/com/mrb/MethodOrderedByNameTest.java
@@ -0,0 +1,30 @@
+package br.com.mrb;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MethodOrderedByNameTest {
+
+    @Test
+    void testC(){
+        System.out.println("Running Teste C");
+    }
+
+    @Test
+    void testD(){
+        System.out.println("Running Teste D");
+    }
+    @Test
+    void testA(){
+        System.out.println("Running Teste A");
+    }
+
+    @Test
+    void testB(){
+        System.out.println("Running Teste B");
+    }
+
+
+}
