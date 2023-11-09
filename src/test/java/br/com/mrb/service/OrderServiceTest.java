d786665a393d343b203fc1737cc00e8a4a6634a1
mauro <mrb0305@gmail.com>
2023-11-09 15:15:24 -0300

    Mockito Annotaion

---
+++ src/test/java/br/com/mrb/service/OrderServiceTest.java
@@ -0,0 +1,35 @@
+package br.com.mrb.service;
+
+import br.com.mrb.model.Order;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+
+public class OrderServiceTest {
+
+    private final OrderService service = new OrderService();
+    private final UUID defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
+    private final LocalDateTime defaultLocalDateTime = LocalDateTime.of(2023, 7, 4, 15, 50);
+    @DisplayName("Should include random OrderId when no OrderId exists")
+    @Test
+    void testShouldIncludeRandonOrderId_When_NoOrderIdExists() {
+        //Given / Arrange
+        try(MockedStatic<UUID> mockedStatic = mockStatic(UUID.class)) {
+            mockedStatic.when(UUID::randomUUID).thenReturn(defaultUuid);
+            //When / Act
+            Order result = service.createOrder("MacBook Pro", 2L, null);
+
+            // Then / Assert
+            assertEquals(defaultUuid.toString(), result.getId());
+        }
+
+        //Then / Assert
+    }
+}
